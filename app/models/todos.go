48c7bb1238fed65347f36c0a2cd413d99c05dc74
tokatu4561 <ryobsk4561@gmail.com>
2022-06-14 17:24:05 -0700

    get todo

--- app/models/todos.go
+++ app/models/todos.go
@@ -22,3 +22,18 @@ func (u *User) CreateTodo(content string) (err error) {
 
 	return err
 }
+
+func GetTodo(id int) (todo Todo, err error) {
+	todo = Todo{}
+	cmd := `select id, content, user_id, create_at
+	from todos where id = ?`
+
+	err = DB.QueryRow(cmd, id).Scan(
+		&todo.ID,
+		&todo.Content,
+		&todo.UserID,
+		&todo.CreatedAt,
+	)
+
+	return todo, err
+}
