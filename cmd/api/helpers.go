25277c759c8bec24c749a7a09696943f21d2c0f6
tokatu4561 <ryobsk4561@gmail.com>
2022-07-13 08:00:13 -0700

    write json herper

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,6 +6,25 @@ import (
 	"errors"
 )
 
+func(app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(out)
+
+	return nil
+}
+
 //
 func(app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error  {
 	maxBytes := 1048576
