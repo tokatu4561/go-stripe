f8db40f494978287ffc56f143f81460acf6d28b6
tokatu4561 <ryobsk4561@gmail.com>
2022-07-17 12:19:15 -0700

    forgot password form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -328,3 +328,9 @@ func (app *application) Logout(w http.ResponseWriter, r *http.Request) {
 
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
+
+func (app *application) ForgotPassword(w http.ResponseWriter, r *http.Request){
+	if err := app.renderTemplate(w, r, "forgot-password", &templateData{}); err != nil {
+		app.errorLog.Print(err)
+	}
+}
